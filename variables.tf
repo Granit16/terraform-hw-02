be194aff145bc12197a897d7ad3d6955549bcddc
Granit16 <damirm@rambler.ru>
2024-09-16 22:11:24 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,11 +19,30 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
+
+
+variable "zone_b" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+
+
 variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
+
+
+variable "cidr_b" {
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+
 
 variable "vpc_name" {
   type        = string
@@ -31,11 +50,32 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
+variable "vpc_name_b" {
+  type        = string
+  default     = "develop_b"
+  description = "VPC network & subnet name"
+}
+
+
+
+variable "vm_web_image_family" {
+  type          = string
+  default       = "ubuntu-2004-lts"
+  description   = "Image family name"
+}
+
 
 ###ssh vars
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
   description = "ssh-keygen -t ed25519"
+}
+
+variable "project" {
+  type = list
+}
+
+variable "env" {
+  type = list
 }
