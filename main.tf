a720d2bc2707a199da946c2d984c9f42fe79e8fb
Granit16 <damirm@rambler.ru>
2024-09-16 22:09:37 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
+
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
@@ -9,33 +10,74 @@ resource "yandex_vpc_subnet" "develop" {
 }
 
 
-data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = var.vpc_name_b
+  zone           = var.zone_b
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.cidr_b
 }
-resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standart-v4"
+
+
+
+data "yandex_compute_image" "ubuntu" {
+  family = var.vm_web_image_family
+}
+
+resource "yandex_compute_instance" "platform-web" {
+  name        = local.vm1_name
+  platform_id = var.vm_web_platform
+
   resources {
-    cores         = 1
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vms_resources.web.cores
+    memory        = var.vms_resources.web.memory
+    core_fraction = var.vms_resources.web.core_fraction
   }
+
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
     }
   }
+
   scheduling_policy {
     preemptible = true
   }
+
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
   }
 
-  metadata = {
-    serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  metadata = var.metadata
+
+}
+
+
+resource "yandex_compute_instance" "platform-db" {
+  name        = local.vm2_name
+  platform_id = var.vm_db_platform
+
+  resources {
+    cores         = var.vms_resources.web.cores
+    memory        = var.vms_resources.web.memory
+    core_fraction = var.vms_resources.web.core_fraction
   }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = var.metadata
 
 }
